eb4bc66dadcbb83978281838306000c51e04d489
DESKTOP-59UIOGL\Hoa <duongnguyen2802@gmail.com>
2018-05-27 16:00:30 +0700

    database script

---
+++ databaseScript.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `quanlyhanghoa`.`khachhang` 
+             ( 
+                          makh VARCHAR(500) NOT NULL PRIMARY KEY, 
+                          tenkh TEXT, 
+                          diachi TEXT, 
+                          dienthoai TEXT, 
+                          email TEXT, 
+             );
+
+
+
+             
